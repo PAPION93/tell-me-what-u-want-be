b979227035fa8976a2da7bdc5ec949830b868380
papion <sjun1319@gmail.com>
2020-09-20 20:27:03 +0900

    임시 request 소스 적용

--- laravel-project/app/Http/Controllers/Api/V1/RestaurantController.php
+++ laravel-project/app/Http/Controllers/Api/V1/RestaurantController.php
@@ -15,7 +15,30 @@ class RestaurantController extends Controller
      */
     public function index(RestaurantRequest $request)
     {
-        echo 'Done';
+        $client_id = "sOCY0oNpt0S8nLDN38Wp";
+        $client_secret = "GtW_LGRrE3";
+        $encText = urlencode($request->input('query'));
+        $url = "https://openapi.naver.com/v1/search/local?query=".$encText."&display=".$request->input('display')."&start=".$request->input('start'); // json 결과
+        //  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // xml 결과
+        $is_post = false;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, $is_post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $headers = array();
+        $headers[] = "X-Naver-Client-Id: ".$client_id;
+        $headers[] = "X-Naver-Client-Secret: ".$client_secret;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $response = curl_exec($ch);
+        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+        if ($status_code == 200) {
+            echo $response;
+        } else {
+            echo "Error 내용:".$response;
+        }
     }
 
     /**
