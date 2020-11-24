fd8706e77615b17968aec55ee759fcd0d1b30a00
PAPION93~ <sjun1319@gmail.com>
2020-11-24 17:56:38 +0900

    Add LikeFactory and Seeder

--- laravel-project/database/seeders/DatabaseSeeder.php
+++ laravel-project/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Image;
+use App\Models\Like;
 use App\Models\Restaurant;
 use Illuminate\Database\Seeder;
 
@@ -23,6 +25,18 @@ class DatabaseSeeder extends Seeder
                     ->count(5)
                     ->create([
                         'restaurant_id' => $restaurant->id
+                    ]);
+            });
+
+        User::factory()
+            ->count(10)
+            ->create()
+            ->each(function ($user) {
+                Like::factory()
+                    ->count(3)
+                    ->create([
+                        'restaurant_id' => Restaurant::all()->random()->id,
+                        'user_id' => User::all()->random()->id,
                     ]);
             });
     }
