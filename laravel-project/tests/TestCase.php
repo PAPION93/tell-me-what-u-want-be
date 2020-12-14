31fad46fd0ce1cd9a87ca8da51fd09a9bd458cc6
PAPION93 <sjun1319@gmail.com>
2020-12-14 21:12:26 +0900

    Update TestCast

--- laravel-project/tests/TestCase.php
+++ laravel-project/tests/TestCase.php
@@ -2,18 +2,22 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Faker\Factory as Faker;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Faker\Factory as Faker;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, DatabaseMigrations, DatabaseTransactions;
+    use CreatesApplication;
+    use DatabaseMigrations;
+    use DatabaseTransactions;
 
     protected $faker;
 
-    public function setUp() : void
+    public function setUp(): void
     {
         parent::setUp();
         $this->faker = Faker::create();
@@ -23,5 +27,60 @@ abstract class TestCase extends BaseTestCase
     {
         $this->artisan('migrate:reset');
         parent::tearDown();
+    }
+
+    /**
+     * Return an admin user
+     * @return User $admin
+     */
+    protected function admin($overrides = [])
+    {
+        $admin = $this->user($overrides);
+        $admin->roles()->attach(
+            factory(Role::class)->states('admin')->create()
+        );
+
+        return $admin;
+    }
+
+    /**
+     * Return an user
+     * @return User
+     */
+    protected function user($overrides = [])
+    {
+        return User::factory()->create();
+    }
+
+    /**
+     * Acting as an admin
+     */
+    protected function actingAsAdmin($api = null)
+    {
+        $this->actingAs($this->admin(), $api);
+
+        return $this;
+    }
+
+    /**
+     * Acting as an user
+     */
+    protected function actingAsUser()
+    {
+        $user = $this->user();
+        $token = JWTAuth::fromUser($user);
+        $this->withHeaders(['Authorization' => "Bearer {$token}"]);
+        $this->actingAs($user);
+
+        return $this;
+    }
+
+    public function actingAs($user, $driver = null)
+    {
+        $token = JWTAuth::fromUser($user);
+        $this->withHeader('Authorization', "Bearer {$token}");
+        parent::actingAs($user);
+
+        return $this;
     }
 }
